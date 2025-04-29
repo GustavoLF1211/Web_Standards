20c76a8af03508801da23c4e56d217e9fc2ff21e
Take_Mashido.dev <gustavo.lopesfrancisco.pfn@gmail.com>
2025-04-28 22:47:43 -0300

    teste-contador-js
    prática de html e css, aplicando js

--- web-2804/atividade-6 copy/script.js
+++ web-2804/atividade-6 copy/script.js
@@ -1,24 +1,49 @@
-let contador = 0
+let contador = 0;
+let numerosorte = 0;
 
-    const contadorElement = document.getElementById('contador')
-    const adicionarElement = document.getElementById('adicionar')
-    const retirarElement = document.getElementById('retirar')
-    const zeroElement = document.getElementById('zero')
+const contadorElement = document.getElementById('contador');
+const adicionarElement = document.getElementById('adicionar');
+const retirarElement = document.getElementById('retirar');
+const zeroElement = document.getElementById('zero');
 
-        adicionarElement.addEventListener
-            ('click', () =>
-                {contador++
-                    contadorElement.textContent = contador;
-                });
-        
-        retirarElement.addEventListener
-            ('click', () =>
-                {contador--
-                    contadorElement.textContent = contador;
-                });
+const numerosorteElement = document.getElementById('numerosorte');
+const adicionarsorteElement = document.getElementById('adicionarsorte');
+const retirarsorteElement = document.getElementById('retirarsorte');
+const zerosorteElement = document.getElementById('zerosorte');
 
-        zeroElement.addEventListener
-            ('click', () =>
-                {contador = 0
-                    contadorElement.textContent = contador;
-                });
+adicionarElement.addEventListener('click', () => {
+    if(contador<10){
+        contador++
+    }
+    if(numerosorte>4){
+        contador++;
+    }
+    contadorElement.textContent = contador;
+});
+
+retirarElement.addEventListener('click', () => {
+        contador--;
+    contadorElement.textContent = contador;
+});
+
+zeroElement.addEventListener('click', () => {
+        contador = 0;
+    contadorElement.textContent = contador;
+});
+
+adicionarsorteElement.addEventListener('click', () => {
+    if(numerosorte<10){
+        numerosorte++;
+    }
+    numerosorteElement.textContent = numerosorte;
+});
+
+retirarsorteElement.addEventListener('click', () => {
+        numerosorte--
+    numerosorteElement.textContent = numerosorte;
+});
+
+zerosorteElement.addEventListener('click', () => {
+    numerosorte = 0;
+    numerosorteElement.textContent = numerosorte;
+});
